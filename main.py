744926eb465cc3151b113ca1fd7c7f7629d409f6
girnal82 <grinal82@gmail.com>
2022-07-03 20:39:10 +0300

    minor fix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from unicodedata import name
 from application.salary import calculate_salary
 from application.people import get_employees
 from datetime import datetime
 
 dt = datetime.now()
 
-if __name__ == "__main__":
+
+def main():
     calculate_salary()
     print('today is:', dt)
     get_employees()
+
+
+if __name__ == "__main__":
+    main()
